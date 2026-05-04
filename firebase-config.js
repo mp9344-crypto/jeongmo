5deb1d40d4422e5c301c56bfae29081ecdb71ca2
Minjun-Park2000 <mp9344@gmail.com>
2026-05-03 18:44:08 -0700

    Re-track firebase-config.js (protected by API key domain restriction)

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyC5WaYcQEkmYrDJju59n5vzusBCLYfKnZs",
+    authDomain: "jeongmo-app.firebaseapp.com",
+    projectId: "jeongmo-app",
+    storageBucket: "jeongmo-app.firebasestorage.app",
+    messagingSenderId: "196820521337",
+    appId: "1:196820521337:web:677f8b4b6f191996095fa8"
+};
