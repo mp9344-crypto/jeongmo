2cd0ee510a141eb759945743e041cd29eee7b2ed
Minjun-Park2000 <mp9344@gmail.com>
2026-05-03 17:51:12 -0700

    Add .gitignore, untrack firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-// firebase-config.js
-const firebaseConfig = {
-    apiKey: "AIzaSyC5WaYcQEkmYrDJju59n5vzusBCLYfKnZs",
-    authDomain: "jeongmo-app.firebaseapp.com",
-    projectId: "jeongmo-app",
-    storageBucket: "jeongmo-app.firebasestorage.app",
-    messagingSenderId: "196820521337",
-    appId: "1:196820521337:web:677f8b4b6f191996095fa8"
-};
